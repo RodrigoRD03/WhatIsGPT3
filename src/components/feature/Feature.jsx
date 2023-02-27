e1058fe345de78e2ed5b174e9b431a2b11955524
Rodrigo Rodriguez <Roddrigo2003cod@gmail.com>
2023-02-26 21:54:14 -0600

    Subiendo Archivos

---
+++ src/components/feature/Feature.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './feature.css'
+
+const Feature = ({ tittle, text }) => {
+  return (
+    <div className='gpt3__features-container__feature'>
+        <div className='gpt3__features-container__feature-tittle'>
+          <div/>
+          <h1>{tittle}</h1>
+        </div>
+        <div className='gpt3__features-container__feature-text'>
+          <p>{text}</p>
+        </div>
+    </div>
+  )
+}
+
+export default Feature
